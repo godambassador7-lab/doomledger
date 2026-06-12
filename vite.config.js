b4f17764ae90c70b98c96235dda5dbf73918b99e
godambassador7@gmail.com <your.email@example.com>
2026-06-12 19:18:09 -0400

    Deploy Vite app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/doomledger/' : '/',
   plugins: [react()],
   server: {
     proxy: {
       '/api': 'http://127.0.0.1:5174',
     },
   },
-})
+}))
